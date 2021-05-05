044dd6f7cb7d4effc2f6e075a54a715c3f9136a6
RominaKavi <Romishkavi@gmail.com>
2021-05-05 23:40:32 +0430

    new repository

---
+++ 3.sh
@@ -0,0 +1,8 @@
+/Documents/test$ git init
+Reinitialized existing Git repository in /home/romina/Documents/test/.git/
+/Documents/test$ git add .
+git commit -m "new repository"
+git remote add origin https://github.com/RominaKavi/Test.git
+fatal: remote origin already exists.
+git remote -v
+git push origin master
